56f07f3116235fba3ef0ea88c9e42d7d0cbba794
michaelblakeferguson <71854226+michaelblakeferguson@users.noreply.github.com>
2021-09-16 12:24:24 -0500

    Update Euler28.cpp

--- #26-50 (Ada, C++, FORTRAN)/C++/Euler28.cpp
+++ #26-50 (Ada, C++, FORTRAN)/C++/Euler28.cpp
@@ -1,6 +1,6 @@
-// Euler28 ~ Number spiral diagonals
-
 /////////////////////////////////////////////////////////////////////
+// Euler28 ~ Number spiral diagonals
+//
 // Problem Statement:
 // 
 // Starting with the number 1 and moving to the right in a clockwise
@@ -26,12 +26,30 @@
 // Each diagonal follows the same pattern of increasing the change
 // between each value by 8. Thus we can loop through each diagonal
 // and increment up to the 500th element.
+//
+//                       43 44 45 46 47 48 49 
+//                       42 21 22 23 24 25 26
+//                       41 20  7  8  9 10 27
+//                       40 19  6  1  2 11 28
+//                       39 18  5  4  3 12 29
+//                       38 17 16 15 14 13 30
+//                       37 36 35 34 33 32 31
 // 
-//                          21 22 23 24 25
-//                          20  7  8  9 10
-//                          19  6  1  2 11
-//                          18  5  4  3 12
-//                          17 16 15 14 13
+// e.g. bottom-right diagonal: 1  --> 3  (+2)
+//                             3  --> 13 (+10)
+//                             13 --> 31 (+18)
+// 
+//      bottom-left diagonal:  1  --> 5  (+4)
+//                             5  --> 17 (+12)
+//                             17 --> 37 (+20) 
+// 
+//      top-left diagonal:     1  --> 7  (+6)
+//                             7  --> 21 (+14)
+//                             21 --> 43 (+22) 
+// 
+//      top-right diagonal:    1  --> 9  (+8)
+//                             9  --> 25 (+16)
+//                             25 --> 49 (+24)
 // 
 /////////////////////////////////////////////////////////////////////
 
@@ -62,4 +80,3 @@ int main()
     cout << "Solution: " << (sum[0] + sum[1] + sum[2] + sum[3] + 1) << endl << endl;
     cout << "Program completed in " << start - end << " seconds.";
 }
-
