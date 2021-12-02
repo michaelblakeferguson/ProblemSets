53137d0662de4d7d880067c867c97756dde7c70f
michaelblakeferguson <71854226+michaelblakeferguson@users.noreply.github.com>
2021-12-02 06:32:37 -0600

    Update Euler40.f90

--- #26-50 (Ada, C++, FORTRAN)/FORTRAN/Euler40.f90
+++ #26-50 (Ada, C++, FORTRAN)/FORTRAN/Euler40.f90
@@ -30,21 +30,48 @@ program Euler40
 	
 	implicit none
 	integer :: time_start, time_finish, clock_finish, solution = 0
-	integer :: i = 0
-	character(len=:), allocatable :: champ
+	integer :: i = 0, length = 0, prev_length = 0 
+	logical :: aNotSet = .true., bNotSet = .true., cNotSet = .true.
+	logical :: dNotSet = .true., eNotSet = .true., fNotSet = .true.
+	logical :: gNotSet = .true.
+	character :: a = "1",b,c,d,e,f,g
 	character(7) :: num
 	
 	call SYSTEM_CLOCK(time_start)
 	
-	champ = ""
-	do while (i <= 185184)
+	do while (length <= 1000000)
 		i = i + 1
 		write(num,'(I0)') i
-		champ = champ // trim(num)
+		length = prev_length + LEN(trim(num))
+		if(length > 10 .and. bNotSet) then
+			b = num((10-prev_length):(10-prev_length))
+			bNotSet = .false.
+		end if
+		if(length > 100 .and. cNotSet) then
+			c = num((100-prev_length):(100-prev_length))
+			cNotSet = .false.
+		end if
+		if(length > 1000 .and. dNotSet) then
+			d = num((1000-prev_length):(1000-prev_length))
+			dNotSet = .false.
+		end if
+		if(length > 10000 .and. eNotSet) then
+			e = num((10000-prev_length):(10000-prev_length))
+			eNotSet = .false.
+		end if
+		if(length > 100000 .and. fNotSet) then
+			f = num((100000-prev_length):(100000-prev_length))
+			fNotSet = .false.
+		end if
+		if(length > 1000000 .and. gNotSet) then
+			g = num((1000000-prev_length):(1000000-prev_length))
+			gNotSet = .false.
+		end if
+		prev_length = length
 	end do
 	
-	solution = (ICHAR(champ(1:1))-48) * (ICHAR(champ(10:10))-48) * (ICHAR(champ(100:100))-48) * (ICHAR(champ(1000:1000))-48) * &
-	           (ICHAR(champ(10000:10000))-48) * (ICHAR(champ(100000:100000))-48) * (ICHAR(champ(1000000:1000000))-48)
+	solution = (ICHAR(a)-48)*(ICHAR(b)-48)*(ICHAR(c)-48)*(ICHAR(d)-48)* &
+	           (ICHAR(e)-48)*(ICHAR(f)-48)*(ICHAR(g)-48)
 
 	call SYSTEM_CLOCK(time_finish)
 	clock_finish = time_finish - time_start
