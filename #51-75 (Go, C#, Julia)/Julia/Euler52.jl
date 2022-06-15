b3e92e80e8b61ab4fc6bacf24b4c6629f8a93925
michaelblakeferguson <71854226+michaelblakeferguson@users.noreply.github.com>
2022-06-15 15:23:02 -0500

    Update Euler52.jl

--- #51-75 (Go, C#, Julia)/Julia/Euler52.jl
+++ #51-75 (Go, C#, Julia)/Julia/Euler52.jl
@@ -1,3 +1,26 @@
+#####################################################################
+## Euler 52 ~ Permuted multiples
+##
+## Problem Statement:
+## 
+## It can be seen that the number, 125874, and its double, 251748,
+## contain exactly the same digits, but in a different order.
+## 
+## Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x,
+## and 6x, contain the same digits.
+## 
+#####################################################################
+
+
+#####################################################################
+## Algorithm Explanation:
+## 
+## Iterates through values of x checking if it contains the same
+## number of digits for each numeral in the 2x, 3x, 4x, 5x, & 6x
+## cases.
+##
+#####################################################################
+
 using BenchmarkTools
 
 function isPerm(x::Int, y::Int)
