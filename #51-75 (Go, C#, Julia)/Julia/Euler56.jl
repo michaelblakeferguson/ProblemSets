3677df9684f2134c905b0ffcf9c47827b4fc7226
michaelblakeferguson <71854226+michaelblakeferguson@users.noreply.github.com>
2022-06-16 16:06:52 -0500

    Create Euler56.jl

---
+++ #51-75 (Go, C#, Julia)/Julia/Euler56.jl
@@ -0,0 +1,62 @@
+#####################################################################
+## Euler 56 ~ Powerful digit sum
+##
+## Problem Statement:
+## 
+## A googol (10^100) is a massive number: one followed by one-hundred
+## zeros; 100^100 is almost unimaginably large: one followed by
+## two-hundred zeros. Despite their size, the sum of the digits
+## in each number is only 1.
+##
+## Considering natural numbers of the form, ab, where a, b < 100,
+## what is the maximum digital sum?
+## 
+#####################################################################
+
+
+#####################################################################
+## Algorithm Explanation:
+## 
+## Loops through 1 to 100 for both a & b, calculating a^b and then
+## storing as a bigInt. Then converts to a string and adds up the
+## digit at each index to find the total. Solution is set to the
+## largest value found during this iteration.
+##
+#####################################################################
+
+using BenchmarkTools
+
+solution = 0
+a = big"1"
+b = big"1"
+c = big"0"
+
+function main()
+
+	while a < 100
+		while b < 100
+			c = a^b
+			s = string(c)
+			total = 0
+			for i in 1:length(s)
+				total = total + Int(s[i]) - 48 
+			end
+			if total > solution
+				global solution = total
+			end
+			global b = b + 1
+		end
+		global b = 1
+		global a = a + 1
+	end
+end
+
+@elapsed begin
+main()
+end
+
+#run @elapsed twice to ignore compilation overhead
+t = @elapsed main()
+print("Solution: ", solution)
+t = t * 1000;
+print("\n\nProgram completed in ", round.(t; sigdigits=5), " milliseconds.")
